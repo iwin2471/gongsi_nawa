2111e23fdaae8e12c8eec4019f8b1becec53cd30
iwin2471 <iwin12372@gmail.com>
2018-06-03 18:55:41 +0900

    start project

---
+++ app/src/main/java/com/example/iwin2/myapplication/splash.java
@@ -0,0 +1,13 @@
+package com.example.iwin2.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
